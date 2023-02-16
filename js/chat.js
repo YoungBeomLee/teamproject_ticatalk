ff4c11f1675c674d621f6b8b9b12a1428025162a
ejin1018 <amy1018@naver.com>
2023-02-16 15:01:47 +0900

    230216 이진 챗톡 인덱스에 추가

--- js/chat.js
+++ js/chat.js
@@ -1,15 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-let chat = document.querySelector('iframe');
-let chatcircle = document.querySelector('.chat-button');
-chatcircle.addEventListener("click",function(){
-  if(chat.style.display=="block"){
-    chat.style.display="none"
-  } else{
-    chat.style.display="block"
-    
-  }
-  
-    
-  })
-
+  let chat = document.querySelector('iframe');
+  let chatcircle = document.querySelector('.chat-button');
+  chatcircle.addEventListener("click",function(){
+    if(chat.style.display=="block"){
+      chat.style.display="none"
+    } else{
+      chat.style.display="block"
+    }
+  });
 });
